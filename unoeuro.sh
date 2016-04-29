9d7f5ae590039fb4617329a6ec8dbd3d0e03ff34
tynor88 <tynor88@users.noreply.github.com>
2016-04-29 10:25:26 +0200

    fix some issues

--- unoeuro.sh
+++ unoeuro.sh
@@ -3,18 +3,18 @@
 if [ -f /config/unoeuro.new.conf ]; then
   tr -d '\r' < /config/unoeuro.new.conf > /tmp/unoeuro.new.conf
   . /tmp/unoeuro.new.conf
-  RESPONSE=`curl -s -L "https://api.unoeuro.com/ddns.php?apikey=$APIKEY&domain=$DOMAIN&hostname=$HOSTNAME"`
-  if [ "$RESPONSE" = "good" ]; then
-    echo "Your IP was updated for domain: $DOMAIN hostname: $HOSTNAME at "$(date)
+  RESPONSE=$(curl -s -L "https://api.unoeuro.com/ddns.php?apikey=$APIKEY&domain=$DOMAIN&hostname=$HOSTNAME")
+  if [[ "$RESPONSE" == "good*" ]]; then
+    echo "[$(date)] Your IP was updated (${RESPONSE#*good }) for domain: $DOMAIN hostname: $HOSTNAME"
   elif [ "$RESPONSE" = "nochg" ]; then
-    echo "The record is already set to the IP given " $(date)
+    echo "[$(date)] The record is already set to the IP given"
   elif [ "$RESPONSE" = "badauth" ]; then
-    echo "Invalid login (ApiKey) "$(date)
-  elif [ "$RESPONSE" = "dnserr" ]; then
-    echo "Error returned from UnoEuro, please check your settings "$(date)
+    echo "[$(date)] Invalid login (ApiKey)"
+  elif [[ "$RESPONSE" == "dnserr*" ]]; then
+    echo "[$(date)] Error returned from UnoEuro, please check your settings"
   else 
-    echo "Something went wrong, please check your settings "$(date)
+    echo "[$(date)] Something went wrong, please check your settings"
   fi
 else
-  echo "Please enter your domain, hostname and apikey as environment variables in your docker run command. See info box for details."
+  echo "[$(date)] Please enter your domain, hostname and apikey as environment variables in your docker run command. See info box for details"
 fi
