e62dfdd3a66920c51039a2893f66dbc92f27c968
tynor88 <tynor88@users.noreply.github.com>
2016-04-29 19:07:16 +0200

    Remove date from echo

--- unoeuro.sh
+++ unoeuro.sh
@@ -5,16 +5,16 @@ if [ -f /config/unoeuro.new.conf ]; then
   . /tmp/unoeuro.new.conf
   RESPONSE=$(curl -s -L "https://api.unoeuro.com/ddns.php?apikey=$APIKEY&domain=$DOMAIN&hostname=$HOSTNAME")
   if [[ "$RESPONSE" == good* ]]; then
-    echo "[$(date)] Your IP was updated (${RESPONSE#*good }) for domain: $DOMAIN hostname: $HOSTNAME"
+    echo "IP was updated (${RESPONSE#*good }) for domain: $DOMAIN hostname: $HOSTNAME"
   elif [ "$RESPONSE" = "nochg" ]; then
-    echo "[$(date)] The record is already set to the IP given"
+    echo "The record is already set to the IP given"
   elif [ "$RESPONSE" = "badauth" ]; then
-    echo "[$(date)] Invalid login (ApiKey)"
+    echo "Invalid ApiKey ($APIKEY)"
   elif [[ "$RESPONSE" == dnserr* ]]; then
-    echo "[$(date)] Error returned from UnoEuro: $RESPONSE"
+    echo "Error returned from UnoEuro: $RESPONSE"
   else 
-    echo "[$(date)] Something went wrong, please check your settings"
+    echo "Something went wrong, please check your settings"
   fi
 else
-  echo "[$(date)] Please enter your domain, hostname and apikey as environment variables in your docker run command. See info box for details"
+  echo "Please enter your domain, hostname and apikey as environment variables in your docker run command. See info box for details"
 fi
