4abb9c219f7a3351d59ca7da8a02c64b1617200b
tynor88 <tynor88@users.noreply.github.com>
2016-04-29 20:11:43 +0200

    cleanup

--- firstrun.sh
+++ firstrun.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-  rm -f /config/unoeuro.new.conf
+  rm -f /config/unoeuro.conf
   # Check to make sure the domain, hostname and apikey are set
   if [ -z "$DOMAIN" ] || [ -z "$HOSTNAME" ] || [ -z "$APIKEY" ]; then
     echo "Please pass your domain, hostname and apikey as environment variables in your docker run command. See docker info for more details."
-    if [ -f /config/unoeuro.conf ]; then
-      echo "Retrieving variables from old unoeuro.conf"
-      tr -d '\r' < /config/unoeuro.conf > /tmp/unoeuro.conf
-      . /tmp/unoeuro.conf
-      echo -e "DOMAIN=$DOMAIN \nHOSTNAME=$HOSTNAME \nAPIKEY=$APIKEY \n" > /config/unoeuro.new.conf
-    fi
+    exit 1
   else
     echo "Retrieving domain, hostname and apikey from the environment variables"
-    echo -e "DOMAIN=$DOMAIN \nHOSTNAME=$HOSTNAME \nAPIKEY=$APIKEY \n" > /config/unoeuro.new.conf
+    echo -e "DOMAIN=$DOMAIN \nHOSTNAME=$HOSTNAME \nAPIKEY=$APIKEY \n" > /config/unoeuro.conf
   fi
   
   echo "Fixing permissions"
