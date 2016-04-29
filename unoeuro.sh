fbf0b50ac5642528fe0c0bc7330274a1eefc0193
tynor88 <tynor88@users.noreply.github.com>
2016-04-29 08:56:10 +0200

    Create unoeuro.sh

---
+++ unoeuro.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -f /config/unoeuro.new.conf ]; then
+  tr -d '\r' < /config/unoeuro.new.conf > /tmp/unoeuro.new.conf
+  . /tmp/unoeuro.new.conf
+  RESPONSE=`curl -s "https://api.unoeuro.com/ddns.php?apikey=$APIKEY&domain=$DOMAIN&hostname=$HOSTNAME"`
+  if [ "$RESPONSE" = "good" ]; then
+    echo "Your IP was updated for domain: $DOMAIN hostname: $HOSTNAME at "$(date)
+  elif [ "$RESPONSE" = "nochg" ]; then
+    echo "The record is already set to the IP given " $(date)
+  elif [ "$RESPONSE" = "badauth" ]; then
+    echo "Invalid login (ApiKey) "$(date)
+  elif [ "$RESPONSE" = "dnserr" ]; then
+    echo "Error returned from UnoEuro, please check your settings "$(date)
+  else 
+    echo "Something went wrong, please check your settings "$(date)
+  fi
+else
+  echo "Please enter your domain, hostname and apikey as environment variables in your docker run command. See info box for details."
+fi
