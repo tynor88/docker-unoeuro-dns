3c8ff0b45721a3a2bcaf5158338433bbf20ecc8e
Thomas Tynor <tynor@hotmail.com>
2016-07-10 17:06:11 +0200

    send pushbullet notifications
    cleanup
    send notifications for unoeuro api errors

--- app/script/unoeuro.sh
+++ app/script/unoeuro.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
+#Load configuration
 . /config/unoeuro.conf
-RESPONSE=$(curl -s -L "https://api.unoeuro.com/ddns.php?apikey=$APIKEY&domain=$DOMAIN&hostname=$HOSTNAME")
-if [[ "$RESPONSE" == good* ]]; then
-  UPDATE_MESSAGE="IP was updated (${RESPONSE#*good }) for domain: $DOMAIN hostname: $HOSTNAME"
-  echo $UPDATE_MESSAGE
+
+UNOEURO_API_URL="https://api.unoeuro.com/ddns.php?apikey=$APIKEY&domain=$DOMAIN&hostname=$HOSTNAME"
+PUSHBULLET_API_URL="https://api.pushbullet.com/v2/pushes"
+
+send_pushbullet () {
   if [[ ! -v "$PUSHBULLET_ACCESS_TOKEN" ]]; then
-    RESPONSE=$(curl -s -L --header "Access-Token: $PUSHBULLET_ACCESS_TOKEN" --header "Content-Type: application/json" --data-binary "{\"body\":\"$UPDATE_MESSAGE\",\"title\":\"IP Updated\",\"type\":\"note\"}" --request POST "https://api.pushbullet.com/v2/pushes")
+    RESPONSE_PUSHBULLET=$(curl -s -L --header "Access-Token: $PUSHBULLET_ACCESS_TOKEN" --header "Content-Type: application/json" --data-binary "{\"body\":\"$1\",\"title\":\"$2\",\"type\":\"note\"}" --request POST "$PUSHBULLET_API_URL")
+    if [[ "$RESPONSE_PUSHBULLET" == {\"error* ]]; then
+      echo "Could not send Pusbullet notification due to an error: $RESPONSE_PUSHBULLET"
+    else
+      echo "Pushbullet notification sent"
+    fi
   fi
-elif [ "$RESPONSE" = "nochg" ]; then
-  echo "The record is already set to the IP given"
-elif [ "$RESPONSE" = "badauth" ]; then
-  echo "Invalid ApiKey ($APIKEY)"
-elif [[ "$RESPONSE" == dnserr* ]]; then
-  echo "Error returned from UnoEuro: $RESPONSE"
+}
+
+RESPONSE_UNOEURO=$(curl -s -L "$UNOEURO_API_URL")
+
+if [[ "$RESPONSE_UNOEURO" == good* ]]; then
+  UPDATE_MESSAGE="IP was updated (${RESPONSE_UNOEURO#*good }) for domain: $DOMAIN hostname: $HOSTNAME"
+  echo $UPDATE_MESSAGE
+  send_pushbullet "$UPDATE_MESSAGE" "IP Updated"
+elif [ "$RESPONSE_UNOEURO" = "nochg" ]; then
+  echo "The DNS record is already set to the IP given"
+elif [ "$RESPONSE_UNOEURO" = "badauth" ]; then
+  ERROR_MESSAGE="Invalid UnoEuro ApiKey ($APIKEY)"
+  echo $ERROR_MESSAGE
+  send_pushbullet "$ERROR_MESSAGE" "Error updating DNS record"
+elif [[ "$RESPONSE_UNOEURO" == dnserr* ]]; then
+  ERROR_MESSAGE="Error returned from UnoEuro when updating DNS record: $RESPONSE_UNOEURO"
+  echo $ERROR_MESSAGE
+  send_pushbullet "$ERROR_MESSAGE" "Error updating DNS record"
 else
-  echo "Something went wrong, please check your settings"
+  ERROR_MESSAGE="Something went wrong when receiving response from UnoEuro, please check your settings: $RESPONSE_UNOEURO"
+  echo $ERROR_MESSAGE
+  send_pushbullet "$ERROR_MESSAGE" "Error updating DNS record"
 fi
